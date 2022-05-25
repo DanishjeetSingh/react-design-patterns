380176586e8afa5ae7e90d1bd2b207a108a24b65
DanishjeetSingh <danishjeetsingh@gmail.com>
2022-05-24 23:39:57 -0400

    layout components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { NumberedList } from './NumberedList';
+import { LargePersonListItem } from './peoples/LargePersonListItem';
+import { SmallPersonListItem } from './peoples/SmallPersonListItem';
+import { LargeProductListItem } from './products/LargeProductListItem';
+import { SmallProductListItem } from './products/SmallProductListItem';
+import { RegularList } from './RegularList';
+
+const people = [{
+	name: 'John Doe',
+	age: 54,
+	hairColor: 'brown',
+	hobbies: ['swimming', 'bicycling', 'video games'],
+}, {
+	name: 'Brenda Smith',
+	age: 33,
+	hairColor: 'black',
+	hobbies: ['golf', 'mathematics'],
+}, {
+	name: 'Jane Garcia',
+	age: 27,
+	hairColor: 'blonde',
+	hobbies: ['biology', 'medicine', 'gymnastics'],
+}];
+
+const products = [{
+	name: 'Flat-Screen TV',
+	price: '$300',
+	description: 'Huge LCD screen, a great deal',
+	rating: 4.5,
+}, {
+	name: 'Basketball',
+	price: '$10',
+	description: 'Just like the pros use',
+	rating: 3.8,
+}, {
+	name: 'Running Shoes',
+	price: '$120',
+	description: 'State-of-the-art technology for optimum running',
+	rating: 4.2,
+}];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <h1>Small Person Item List</h1>
+    <RegularList items={people} resourceName = 'person' itemComponent={SmallPersonListItem} />
+    <h1>Large Person Item List</h1>
+    <RegularList items={people} resourceName = 'person' itemComponent={LargePersonListItem} />
+    <h1>Small Product List Item</h1>
+    <NumberedList items={products} resourceName = 'product' itemComponent={SmallProductListItem} />
+    <h1>Large Product List Item</h1>
+    <NumberedList items={products} resourceName = 'product' itemComponent={LargeProductListItem} />
+    
+    </>
+
+  )
 }
 
 export default App;
---
+++ src/products/LargeProductListItem.js
@@ -0,0 +1,13 @@
+export const LargeProductListItem = ({product}) => {
+    const {name, price, description, rating} = product
+
+    return(
+        <>
+            <h3>{name}</h3>
+            <p>{price}</p>
+            <p>Description:</p>
+            <p>{description}</p>
+            <p>Average Rating: {rating}</p>
+        </>
+    )
+}
